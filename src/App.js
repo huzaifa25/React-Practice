2729600853dd9c60a3e7cf54ec3d8b3112359a51
huzaifacc <huzaifa.web.intern@gmail.com>
2022-04-06 15:22:56 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+//import {Button} from 'react-bootstrap'
+import Header from './Header.js'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+     {/* <h1>Adnan is React Developer</h1> */}
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import {Navbar,Nav,Container,Button,FormControl,Form} from 'react-bootstrap'
+function Header(){
+    return(
+        <div>
+              <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Container>
+  </Navbar>
+        </div>
+    )
+}
+export default Header
