50f2d8dacfefaa8d6bc31132dde235d09b68289d
huzaifacc <huzaifa25dec@gmail.com>
2022-05-16 16:13:50 +0500

    FIx some bugs final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js'
+import Header from './HeaderLink.js'
 import User from './User.js'
 import Register from './Register.js';
 import Login from './Login';
@@ -12,6 +12,7 @@ import Card from './Card';
 import {useState,useEffect} from 'react'
 import IpAddress from './IpAddress';
 import './Card.css';
+import HeaderCard from './HeaderCard';
 //import image from 'https://blog.harlequin.com/wp-content/uploads/2019/12/Virgin-River-poster_forrest_2_with-date-1024x1024.jpg'
 //import {Routes, Route, Link} from 'react-router-dom';
 function App() {
@@ -21,7 +22,8 @@ function App() {
 
   
       <>
-    
+
+      <HeaderCard/>
       <Card cardimg="https://blog.harlequin.com/wp-content/uploads/2019/12/Virgin-River-poster_forrest_2_with-date-1024x1024.jpg"
             title="The Virgin River"
             link="https://www.netflix.com/pk/title/80240027"/>
---
+++ src/HeaderCard.js
@@ -0,0 +1,9 @@
+import './HeaderCard.css';
+function HeaderCard(){
+    return(
+            <div className="header">
+                <span className="Best">BEST TV SHOWS ON</span> <h1>NETFLIX</h1>
+            </div>
+    )
+}
+export default HeaderCard;
